9168f6796883898aafd59667b5f7500c20f651e8
h0353914 <45159732+h0353914@users.noreply.github.com>
2023-07-18 16:13:04 +0800

    Up

--- Assets/腳本/建築.cs
+++ Assets/腳本/建築.cs
@@ -5,31 +5,40 @@ using UnityEngine;
 
 public class 建築 : MonoBehaviour
 {
-    public GameObject obj;
+    public GameObject 椅子;
     // Start is called before the first frame update
     void Start()
     {
 
     }
     bool E_bool = false;
-    GameObject copy;
+    GameObject Temp;
     // Update is called once per frame
     void Update()
     {
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
         if (Input.GetKeyDown(KeyCode.E))
         {
             if (E_bool)
             {
-                Destroy(copy);
+                Destroy(Temp);
                 E_bool = false;
             }
             else
             {
-                copy = Instantiate(obj); 
+                Temp = Instantiate(椅子);
                 E_bool = true;
             }
-
         }
-
+        if (!Input.GetKeyDown(KeyCode.E) && E_bool == true)
+        {
+            if (Physics.Raycast(ray, out hit, 100f))
+            {
+                Temp.transform.localPosition = hit.point; 
+            }           
+        }
     }
 }
--- Assets/腳本/移動.cs
+++ Assets/腳本/移動.cs
@@ -12,32 +12,40 @@ public class 移動 : MonoBehaviour
     bool 目標Collision, 地板Collision;
     public Transform 目標;
     public Transform 地板;
-    public float 速度 = 50;
+    public float MoveSpeed = 0.1f;
+    public float MouseSpeed = 2;
     Rigidbody rBody;
-     CharacterController Cc;
+    CharacterController Cc;
+    float Mouse_X = 0, Mouse_Y = 0;
     void Start()
     {
         //rBody = GetComponent<Rigidbody>();
     }
     void Update()
     {
-        float x = Input.GetAxis("Horizontal") * 速度;
-        float z = Input.GetAxis("Vertical") * 速度;
         //float y = 0;
         //if (ToBoolean(Input.GetAxis("Jump")) && (地板Collision == true))
         //{
         //    y = 300;
         //    地板Collision = false;
         //}
-        transform.transform.Rotate(0, Input.GetAxis("Jump"), 0);
         //rBody.transform.transform.Rotate(0, Input.GetAxis("Jump"), 0);
         //transform.rotation = Quaternion.LookRotation(new(0, Input.GetAxisRaw("Jump"), 0));
-        Cc = GetComponent<CharacterController>();
-        Cc.Move(transform.TransformDirection(new Vector3(x, 0, z)));
-
         //rBody.transform.Translate(new Vector3(x, 0, z));
         //rBody.velocity = new Vector3(0, y, 0);
         //rBody.AddForce(new Vector3(x, y, z));
+        //transform.transform.Rotate(Mouse_Y,Mouse_X , 0, Space.World);
+        //Cc.Move(transform.TransformDirection(new Vector3(x, 0, z)));
+        float x = Input.GetAxis("Horizontal") * MoveSpeed;
+        float z = Input.GetAxis("Vertical") * MoveSpeed;
+        float y = Input.GetAxis("UpDown") * MoveSpeed;
+
+        Cc = GetComponent<CharacterController>();
+        Cc.Move(transform.TransformDirection(new Vector3(x, 0, z)));
+        Cc.Move(new Vector3(0, y, 0));
+        Mouse_X += MouseSpeed * Input.GetAxis("Mouse X"); //水平鼠標偏移
+        Mouse_Y += -1 * MouseSpeed * Input.GetAxis("Mouse Y");//垂直鼠標偏移
+        transform.rotation = Quaternion.Euler(Mouse_Y, Mouse_X, 0);
     }
 
 
