a24dd7b6acb9f7d50cf8c819bc37db443c9f3731
h0353914 <45159732+h0353914@users.noreply.github.com>
2023-07-24 23:48:21 +0800

    Update 建築.cs

--- Assets/腳本/建築.cs
+++ Assets/腳本/建築.cs
@@ -6,53 +6,37 @@ using UnityEngine;
 public class 建築 : MonoBehaviour
 {
     bool E_bool = false;
-    GameObject Temp;
-    GameObject Temp2;
+    GameObject Temp物品;
+    GameObject Temp子彈;
     public float 距離 = 7;
     public GameObject 椅子;
     public GameObject gobj_super;
-    float i = 7;
-    bool 移動bool = false;
+    float 子彈距離 = 7;
+    bool 子彈bool = false;
     void Start()
-    {
-
+    { 
     }
     void Update()
     {
         Key();
-        if (移動bool)
+        Mouse(); 
+        if (子彈bool)
         {
-            //if (Vector3.Distance(transform.position, Temp2.transform.position) > 50)
-            //{
-            //    //Temp2.Describe();
-            //}
-            //else
-            //{
-                i += 0.05f;
-                Temp2.transform.position = Temp2.transform.position + transform.forward * i;
-            //}
+            子彈距離 += 0.05f;
+            
+            Temp子彈.transform.position = transform.position + transform.forward * 子彈距離;
+            if (子彈距離 > 20)
+            {
+                Debug.Log(子彈距離);
+                子彈距離 = 0;
+                子彈bool = false;
+                Destroy(Temp子彈);
+                
+            }
         }
-
     }
-
-    private void Key()
+    private void Mouse()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            if (E_bool)
-            {
-                Destroy(Temp);
-                E_bool = false;
-            }
-            else
-            {
-                Temp = Instantiate(椅子);
-                Temp.transform.parent = gobj_super.transform;//放到父物件
-                E_bool = true;
-            }
-        }
         if (Input.GetMouseButtonDown(0))//左鍵
         {
             E_bool = false;
@@ -61,31 +45,45 @@ public class 建築 : MonoBehaviour
 
         if (Input.GetMouseButtonDown(1))//右鍵
         {
-            i = 0;
-            Temp2 = Instantiate(椅子);
-            移動bool = true;
+            Temp子彈 = Instantiate(椅子);
+            子彈bool = true;
+            
             return;
         }
-
+    }
+    private void Key()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (E_bool)
+            {
+                Destroy(Temp物品);
+                E_bool = false;
+            }
+            else
+            {
+                Temp物品 = Instantiate(椅子);
+                Temp物品.transform.parent = gobj_super.transform;//放到父物件
+                E_bool = true;
+            }
+        }
         if (E_bool == true)
         {
-
             Debug.DrawRay(transform.position, transform.forward * 距離, Color.black);
             if (Physics.Raycast(ray, out hit, 距離))
             {
                 //Debug.Log(hit.collider.tag);
                 if (hit.collider.tag != "物品")
                 {
-                    Temp.transform.position = hit.point;
+                    Temp物品.transform.position = hit.point;
                 }
             }
             else
             {
-                Temp.transform.position = transform.position + transform.forward * 距離;
+                Temp物品.transform.position = transform.position + transform.forward * 距離;
             }
-
         }
-
-
     }
 }
