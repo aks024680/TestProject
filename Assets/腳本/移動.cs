bba64f99eca7f094fc8f62bea9d7df2d20536819
aks024680 <130197640+aks024680@users.noreply.github.com>
2023-08-03 12:31:50 +0800

    Update 移動.cs

--- Assets/腳本/移動.cs
+++ Assets/腳本/移動.cs
@@ -2,26 +2,31 @@
 
 public class 移動 : MonoBehaviour
 {
-    public Transform 目標;
-    public Transform 地板;
     public float MoveSpeed = 0.1f;
     public float MouseSpeed = 2;
     public float UpDownSpeed = 0.1f;
-    CharacterController Cc;
+    //CharacterController Cc;
     float Mouse_X = 0, Mouse_Y = 0;
+    private Rigidbody rBody;
+
     void Start()
     {
-        //rBody = GetComponent<Rigidbody>();
+        rBody = GetComponent<Rigidbody>();
     }
     void Update()
     {
         float x = Input.GetAxis("Horizontal") * MoveSpeed * Time.deltaTime;
         float z = Input.GetAxis("Vertical") * MoveSpeed * Time.deltaTime;
         float y = Input.GetAxis("UpDown") * UpDownSpeed * Time.deltaTime;
-        Cursor.lockState = CursorLockMode.Locked;
-        Cc = GetComponent<CharacterController>();
-        Cc.Move(transform.TransformDirection(new Vector3(x, 0, z)));
-        Cc.Move(new Vector3(0, y, 0));
+        //Cursor.lockState = CursorLockMode.Locked;
+        //Cc = GetComponent<CharacterController>();
+        //Cc.Move(transform.TransformDirection(new Vector3(x, 0, z)));
+        //Cc.Move(new Vector3(0, y, 0));
+
+
+        rBody.transform.Translate(new Vector3(x,y, z));
+        //rBody.velocity = new Vector3(0, y, 0);
+
         Mouse_X += Input.GetAxis("Mouse X") * MouseSpeed * Time.deltaTime;
         Mouse_Y += -Input.GetAxis("Mouse Y") * MouseSpeed * Time.deltaTime;
         transform.rotation = Quaternion.Euler(Mouse_Y, Mouse_X, 0);
